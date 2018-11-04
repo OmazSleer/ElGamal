c95849baa0fc6910be431cb2bc794988542b73e4
Dmitrytxt <dmitrytxt@users.noreply.github.com>
2018-11-04 17:04:55 +0300

    Prime

--- ElGamal.dpr
+++ ElGamal.dpr
@@ -7,6 +7,25 @@ program ElGamal;
 uses
   System.SysUtils;
 
+var
+  p, i: Integer;
+  prime: Boolean;
+
 begin
-  Writeln('Amazasp gay');
+  prime := False;
+  while prime = False do
+  begin
+    prime := True;
+    Writeln('Input p:');
+    Readln(p);
+    for i := 2 to p div 2 do
+    begin
+      if p mod i = 0 then
+      begin
+        prime := False;
+        Break;
+      end;
+    end;
+  end;
 end.
+
